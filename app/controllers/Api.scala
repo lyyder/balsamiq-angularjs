660c5bffbd86f09ea021000a8d1fd380af0eae9a
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-06-28 12:55:06 +0300

    Add beer method sceleton

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -12,22 +12,38 @@ import play.api.libs.json.Json._
 object Api extends Controller {
 
 
-  var beerStorage = Map("red_neck" -> new Beer("red neck", "Red Neck", "100", "Very Good"))
+  var beerStorage = Map("red_neck" -> new Beer("red_neck", "Red Neck", "100", "Very Good"))
 
 
   def beers = Action {
-    Ok(toJson(beerStorage("red_neck")))
+    Ok(toJson(beerStorage values))
   }
+
+  def save = Action { request =>
+    val body = request.body
+    println("body: " + body)
+
+    Ok()
+  }
+
 
 //  def beer(id: String) = Action {
 //    Ok()
 //  }
 
 
-
   case class Beer (id: String, name: String, hops: String, comments: String);
 
     implicit val beerJsonWrite = new Writes[Beer] {
+      def reads(json: JsValue): Beer = Beer(
+        (json \ "id").as[String],
+        (json \ "name").as[String],
+        (json \ "hops").as[String],
+        (json \ "comments").as[String]
+
+      )
+
+
       def writes(b: Beer): JsValue = {
         obj(
           "id" -> b.id,
