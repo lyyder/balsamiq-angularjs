8ac46a90cb360a75eae66e54641a73ae1dd548b2
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-06-28 12:41:35 +0300

    beer json writer

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{JsValue, Writes}
+import play.api.libs.json.Json._
 
 /**
  *
@@ -10,14 +12,30 @@ import play.api.mvc.{Action, Controller}
 object Api extends Controller {
 
 
-  var beerStorage = Map("red_neck" -> "{\"id\" : \"red_neck\", \"name\" : \"Red Neck\", \"hops\": \"1000\", , \"comments\": \"aaawyeah\"}")
+  var beerStorage = Map("red_neck" -> new Beer("red neck", "Red Neck", "100", "Very Good"))
 
 
   def beers = Action {
-    Ok(beerStorage("red_neck"))
+    Ok(toJson(beerStorage("red_neck")))
   }
 
 //  def beer(id: String) = Action {
 //    Ok()
 //  }
+
+
+
+  case class Beer (id: String, name: String, hops: String, comments: String);
+
+    implicit val beerJsonWrite = new Writes[Beer] {
+      def writes(b: Beer): JsValue = {
+        obj(
+          "id" -> b.id,
+          "name" -> b.name,
+          "hops" -> b.hops,
+          "comments" -> b.comments
+        )
+      }
+    }
+
 }
